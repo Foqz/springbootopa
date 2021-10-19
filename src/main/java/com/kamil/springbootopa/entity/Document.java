0af6a8cc67f7912d4f4dba39db28f2a45004b0ac
kamil.fiks <kamil.fiks@canpack.com>
2021-10-19 15:10:54 +0200

    in memory security

--- src/main/java/com/kamil/springbootopa/entity/Document.java
+++ src/main/java/com/kamil/springbootopa/entity/Document.java
@@ -12,4 +12,28 @@ public class Document {
     private Long id;
     private String content;
     private String owner;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
 }
--- src/main/java/com/kamil/springbootopa/entity/Salary.java
+++ src/main/java/com/kamil/springbootopa/entity/Salary.java
@@ -12,4 +12,28 @@ public class Salary {
     @Column(unique = true)
     private String username;
     private double amount;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
 }
